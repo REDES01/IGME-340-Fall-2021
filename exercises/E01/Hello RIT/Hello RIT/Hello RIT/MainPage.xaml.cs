804d645d70aff86af96145b37ff4a7e1f2af81e7
“red's giy desktop” <476591878@qq.com>
2021-08-28 14:33:58 -0400

    finish e01

---
+++ exercises/E01/Hello RIT/Hello RIT/Hello RIT/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Hello_RIT
+{
+    public partial class MainPage: ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var topLabel = new Label();
+            var bottomLabel = new Label();
+
+            topLabel.Text = "Hello";
+            topLabel.FontSize = 36;
+            mainStack.Children.Add(topLabel);
+
+            topLabel.Margin = new Thickness(100, 40, 0, 0);
+
+            bottomLabel.Text = "RIT";
+            bottomLabel.FontSize = 48;
+            bottomLabel.HorizontalTextAlignment = TextAlignment.Center;
+            mainStack.Children.Add(bottomLabel);
+            BackgroundColor = Color.Orange;
+
+            var nameLabel = new Label();
+            nameLabel.Text = "Xuanang Zhao";
+            nameLabel.FontSize = 20;
+            nameLabel.HorizontalTextAlignment = TextAlignment.Center;
+            nameLabel.VerticalOptions = LayoutOptions.EndAndExpand;
+            nameLabel.Margin = new Thickness(0, 0, 0, 50);
+            mainStack.Children.Add(nameLabel);
+        }
+    }
+}
